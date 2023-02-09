d4d1b24f99c6ec593cd69fef5040551c444d0293
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-02-10 01:16:26 +0530

    Create for loop.java

---
+++ for loop.java
@@ -0,0 +1,14 @@
+package arrays;
+public class Test3 {
+    public static void main(String args[]){
+        int n=5;
+
+        for(int i = 0; i<6; i++ ){
+            for(int j = 0; j<=i; j++ ){
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        }
+    }
