4d455c1bbccb75ca248b33bc7daa7cf06ef558b5
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-02-10 01:11:38 +0530

    Create assignment.java

---
+++ assignment.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class Assingnment {
+    
+    public static void main(String args[]){
+        /*  a=2    a+=5     a=a+5;
+        a=2    a-=5     a=a-5;
+        a=2    a*=5     a=a*5;
+        a=2    a/=5     a=a/5;  */
+
+        int a= 10;
+           a += 2;
+           System.out.println("the value of a:"+a);
+        int b= 14;
+         b-=3;
+         System.out.println("the value of b:"+b); 
+         int d= 14;
+         d*=3;
+         System.out.println("the value of d:"+d); 
+         int e= 10;
+         e/=2;
+         System.out.println("the value of e:"+e);   
+    }
+
+}
