7c1732f7c06cf93c2d955973a5e153c696dcd832
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-03-18 12:08:43 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,25 @@
+package day2;
+public class Operator {
+    public static void main(String args[]){
+
+    
+    /*   logical operator
+            and(&&)
+            or(||)
+            not(!)
+
+    */
+
+    int num1 = 19;
+
+    if( num1>=18 && 20>=num1){
+        System.out.println("fresher vote:");
+    }
+    else if(num1>18){
+        System.out.println("elder vote:");
+    }
+    else{
+        System.out.println("you are not eligible to vote:");
+    }
+}
+}
