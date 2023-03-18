0ef09f2aef4f5ce72b77b4c336e2508d6f20b11c
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-03-18 11:55:36 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,36 @@
+package arrays;
+
+ public class Case {
+    public static void main(String args[]){
+        char day =1;
+
+        switch(day){
+
+            case 1:
+                System.out.println("this is sunday");
+                break;
+            
+            case 2:
+                System.out.println("this is monday");
+                break;
+            case 3:
+                System.out.println("this is tuesday");
+                break;
+            case 4:
+                System.out.println("this is wednesday");
+                break;
+            case 5:
+                System.out.println("this is thursday");
+                break;
+            case 6:
+                System.out.println("this is friday");
+                break;
+            case 7:
+                System.out.println("this is saturday:");
+                break;            
+            default:
+                System.out.println("this day in not found:");
+                    break;
+            }
+    }
+}
