c650f5ba6a8de7ba384ca0603d60d9517efb5e2d
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-03-18 12:16:33 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,16 @@
+public class Test5{
+    public static void main(String args[]){
+        
+        int[] nums = {1,2,3,4,5,6,7,8};
+        
+        int sum = 0;
+
+        for(int i=0; i<nums.length; i++){
+            
+            sum = sum + nums[i];
+            
+          
+        }
+        System.out.println(sum);
+    }
+}
