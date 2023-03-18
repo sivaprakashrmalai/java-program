9f0a74f55b12165cfe68ca919433ed37786a8d7b
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-03-18 12:07:48 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,10 @@
+package day2;
+public class Variable{
+    public static void main (String args[]){
+                 double price=2.047165683858612681;
+                 int num=(int)price;
+                 System.out.println(num);
+    }
+
+    
+}
