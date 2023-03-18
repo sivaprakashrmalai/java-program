5ba1873516b2d84479aeccadf40d08f11c3c8da8
sivaprakashrmalai <103348485+sivaprakashrmalai@users.noreply.github.com>
2023-03-18 11:59:48 +0530

    Create for eachloop.java

---
+++ for eachloop.java
@@ -0,0 +1,16 @@
+package arrays.day1;
+
+public class ForEachLoop {
+    public static void main(String args[]){
+        // for each loop:
+        
+
+        int[] arr1 ={1,2,3,4,5,6,7,8,9};
+
+        for(int i : arr1){
+            System.out.println(arr1[i]);
+        }
+        
+    }
+    
+}
